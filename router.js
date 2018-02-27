1cfb39b7a6242496012e40fe14027bd4d1d7d07f
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2018-02-27 21:36:02 +0000

    added feature simple signup

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
-module.exports = function(app) {
-  app.get('/', function(req, res, next) {
+const Authentication = require('./controllers/authentication');
 
-  });
+module.exports = function(app) {
+  app.post('/signup', Authentication.signup);
 
 }
