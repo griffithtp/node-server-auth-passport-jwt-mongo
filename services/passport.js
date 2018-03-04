0113cd59010e65ac0609a0fd1df55628a2f385f5
Griffith Tchenpan <griffith.tchenpan@myworkpapers.com>
2018-03-04 19:43:00 +0000

    added localLogin Passport Local Strategy

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,22 @@ const User = require('../models/user');
 const config = require('../config');
 const jwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const localStrategy = require('passport-local');
+
+const localOptions = { usernameField: 'email'};
+const localLogin = new localStrategy(localOptions, function( email, passport, done) {
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+
+    user.comparePassword(password, function(err, isMatch) {
+      if (err) { return done(err); }
+      if (!isMatch) { return done(null, false); }
+
+      return done(null, user);
+    })
+  })
+})
 
 // setup options for jwt Strategy
 const jwtOptions = {
@@ -26,3 +42,4 @@ const jwtLogin = new jwtStrategy(jwtOptions, function(payload, done) {
 
 // tell passport to use this Strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
